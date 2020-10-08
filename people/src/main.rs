70a9073f44a2c0cd66a4902c89072af58865c78f
Nils Asmussen <nils@os.inf.tu-dresden.de>
2020-10-08 14:18:31 +0200

    Added first solution to people exercise.

--- people/src/main.rs
+++ people/src/main.rs
@@ -1,9 +1,59 @@
 use std::io;
 use std::io::Write;
 
-struct Person {}
+struct Person {
+    name: String,
+    age: i32,
+}
 
-enum Command {}
+impl Person {
+    fn new(name: String) -> Person {
+        Person {
+            name,
+            age: 0,
+        }
+    }
+}
+
+enum Command {
+    Add(String),
+    Age(String, String),
+    Remove(String),
+    Show,
+    Help,
+    Quit,
+}
+
+fn parse(cmd: String) -> Command {
+    let parts: Vec<&str> = cmd.split_whitespace().collect();
+
+    if parts.len() == 0 {
+        panic!("Unknown command");
+    }
+
+    match parts[0] {
+        "add"    => {
+            assert!(parts.len() == 2);
+            Command::Add(parts[1].to_string())
+        }
+
+        "age"    => {
+            assert!(parts.len() == 3);
+            Command::Age(parts[1].to_string(), parts[2].to_string())
+        }
+
+        "rem"    => {
+            assert!(parts.len() == 2);
+            Command::Remove(parts[1].to_string())
+        }
+
+        "show"   => Command::Show,
+        "help"   => Command::Help,
+        "quit"   => Command::Quit,
+
+        _        => panic!("Unknown command"),
+    }
+}
 
 fn get_cmd() -> Command {
     // print prompt (ignore errors)
@@ -12,8 +62,28 @@ fn get_cmd() -> Command {
 
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
-    // TODO parse line and create Command
-    panic!("Not implemented");
+    parse(command)
+}
+
+fn cmd_add(people: &mut Vec<Person>, name: String) {
+    people.push(Person::new(name));
+}
+
+fn cmd_age(people: &mut Vec<Person>, name: String, age: String) {
+    let mut p = people.iter_mut().find(|p| p.name == name).unwrap();
+    p.age = age.parse::<i32>().unwrap();
+}
+
+fn cmd_remove(people: &mut Vec<Person>, name: String) {
+    people.retain(|p| p.name != name);
+}
+
+fn cmd_show(people: &Vec<Person>) {
+    println!("[");
+    for p in people {
+        println!("  {} (Age {})", p.name, p.age);
+    }
+    println!("]");
 }
 
 fn cmd_help() {
@@ -34,6 +104,13 @@ fn main() {
     loop {
         let command = get_cmd();
 
-        // TODO handle command
+        match command {
+            Command::Add(n)         => cmd_add(&mut people, n),
+            Command::Age(n, a)      => cmd_age(&mut people, n, a),
+            Command::Remove(n)      => cmd_remove(&mut people, n),
+            Command::Show           => cmd_show(&people),
+            Command::Help           => cmd_help(),
+            Command::Quit           => break,
+        };
     }
 }
