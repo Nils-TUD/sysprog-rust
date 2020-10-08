098e5857a1f49708aba37a5c97d84905c9b6321a
Nils Asmussen <nils@os.inf.tu-dresden.de>
2020-10-08 13:45:16 +0200

    Added solution to words exercise.

--- words/src/lib.rs
+++ words/src/lib.rs
@@ -1,14 +1,36 @@
 /// Returns the number of upper case letters in `word`
 pub fn count_upper(word: &str) -> usize {
-    // TODO
-    0
+    let mut num = 0;
+    for c in word.chars() {
+        if c.is_uppercase() {
+            num += 1;
+        }
+    }
+    num
+
+    // alternatively:
+    // word.chars().filter(|c| c.is_uppercase()).count()
 }
 
 /// Returns a tuple with the number of words in `text` that contain upper case letters and the total
 /// number of upper case letters
 pub fn count_words_upper(text: &str) -> (usize, usize) {
-    // TODO
-    (0, 0)
+    let mut num = (0, 0);
+    for word in text.split_whitespace() {
+        let upper = count_upper(word);
+        if upper > 0 {
+            num.0 += 1;
+            num.1 += upper;
+        }
+    }
+    num
+
+    // alternatively:
+    // text.split_whitespace()
+    //     .fold((0, 0), |num, word| match count_upper(word) {
+    //         0 => num,
+    //         n => (num.0 + 1, num.1 + n),
+    //     })
 }
 
 #[test]
