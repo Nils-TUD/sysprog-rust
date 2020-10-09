0c02db675c1f190185a541a9a3d3be9feab6fad3
Nils Asmussen <nils@os.inf.tu-dresden.de>
2020-10-09 10:06:51 +0200

    Added proper error handling and other improvements to people exercise.

--- people/src/main.rs
+++ people/src/main.rs
@@ -1,17 +1,29 @@
+use std::fmt;
 use std::io;
 use std::io::Write;
+use std::num::ParseIntError;
 
 struct Person {
     name: String,
-    age: i32,
+    age: Option<i32>,
 }
 
 impl Person {
     fn new(name: String) -> Person {
         Person {
             name,
-            age: 0,
+            age: None,
         }
+    }
+}
+
+impl fmt::Display for Person {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Person({}", self.name)?;
+        if let Some(n) = self.age {
+            write!(f, ", {} years", n)?;
+        }
+        write!(f, ")")
     }
 }
 
@@ -24,69 +36,101 @@ enum Command {
     Quit,
 }
 
-fn parse(cmd: String) -> Command {
-    let parts: Vec<&str> = cmd.split_whitespace().collect();
+#[derive(Debug)]
+enum Error {
+    MissingArg,
+    UnknownCmd,
+    ReadFailed,
+    ParseFailed,
+    NotFound,
+    Exists,
+}
 
+impl From<ParseIntError> for Error {
+    fn from(_: ParseIntError) -> Self {
+        Error::ParseFailed
+    }
+}
+
+fn parse(cmd: String) -> Result<Command, Error> {
+    let parts: Vec<&str> = cmd.split_whitespace().collect();
     if parts.len() == 0 {
-        panic!("Unknown command");
+        return Err(Error::MissingArg);
     }
 
     match parts[0] {
         "add"    => {
-            assert!(parts.len() == 2);
-            Command::Add(parts[1].to_string())
+            if parts.len() != 2 {
+                return Err(Error::MissingArg);
+            }
+            Ok(Command::Add(parts[1].to_string()))
         }
 
         "age"    => {
-            assert!(parts.len() == 3);
-            Command::Age(parts[1].to_string(), parts[2].to_string())
+            if parts.len() != 3 {
+                return Err(Error::MissingArg);
+            }
+            Ok(Command::Age(parts[1].to_string(), parts[2].to_string()))
         }
 
         "rem"    => {
-            assert!(parts.len() == 2);
-            Command::Remove(parts[1].to_string())
+            if parts.len() != 2 {
+                return Err(Error::MissingArg);
+            }
+            Ok(Command::Remove(parts[1].to_string()))
         }
 
-        "show"   => Command::Show,
-        "help"   => Command::Help,
-        "quit"   => Command::Quit,
+        "show"   => Ok(Command::Show),
+        "help"   => Ok(Command::Help),
+        "quit"   => Ok(Command::Quit),
 
-        _        => panic!("Unknown command"),
+        _        => Err(Error::UnknownCmd),
     }
 }
 
-fn get_cmd() -> Command {
+fn get_cmd() -> Result<Command, Error> {
     // print prompt (ignore errors)
     print!("> ");
     io::stdout().flush().ok();
 
     let mut command = String::new();
-    io::stdin().read_line(&mut command).unwrap();
-    parse(command)
+    match io::stdin().read_line(&mut command) {
+        Ok(_)   => parse(command.to_lowercase()),
+        Err(_)  => Err(Error::ReadFailed),
+    }
 }
 
-fn cmd_add(people: &mut Vec<Person>, name: String) {
+fn cmd_add(people: &mut Vec<Person>, name: String) -> Result<(), Error> {
+    if people.iter().find(|p| p.name == name).is_some() {
+        return Err(Error::Exists);
+    }
+
     people.push(Person::new(name));
+    Ok(())
 }
 
-fn cmd_age(people: &mut Vec<Person>, name: String, age: String) {
-    let mut p = people.iter_mut().find(|p| p.name == name).unwrap();
-    p.age = age.parse::<i32>().unwrap();
+fn cmd_age(people: &mut Vec<Person>, name: String, age: String) -> Result<(), Error> {
+    let mut p = people.iter_mut().find(|p| p.name == name).ok_or(Error::NotFound)?;
+    p.age = Some(age.parse::<i32>()?);
+    Ok(())
 }
 
-fn cmd_remove(people: &mut Vec<Person>, name: String) {
+fn cmd_remove(people: &mut Vec<Person>, name: String) -> Result<(), Error> {
+    people.iter().find(|p| p.name == name).ok_or(Error::NotFound)?;
     people.retain(|p| p.name != name);
+    Ok(())
 }
 
-fn cmd_show(people: &Vec<Person>) {
+fn cmd_show(people: &[Person]) -> Result<(), Error> {
     println!("[");
     for p in people {
-        println!("  {} (Age {})", p.name, p.age);
+        println!("  {}", p);
     }
     println!("]");
+    Ok(())
 }
 
-fn cmd_help() {
+fn cmd_help() -> Result<(), Error> {
     println!("The following commands are available:");
     println!("  add <name>");
     println!("  age <name> <age>");
@@ -94,6 +138,7 @@ fn cmd_help() {
     println!("  show");
     println!("  help");
     println!("  quit");
+    Ok(())
 }
 
 fn main() {
@@ -104,13 +149,18 @@ fn main() {
     loop {
         let command = get_cmd();
 
-        match command {
-            Command::Add(n)         => cmd_add(&mut people, n),
-            Command::Age(n, a)      => cmd_age(&mut people, n, a),
-            Command::Remove(n)      => cmd_remove(&mut people, n),
-            Command::Show           => cmd_show(&people),
-            Command::Help           => cmd_help(),
-            Command::Quit           => break,
+        let res = match command {
+            Ok(Command::Add(n))         => cmd_add(&mut people, n),
+            Ok(Command::Age(n, a))      => cmd_age(&mut people, n, a),
+            Ok(Command::Remove(n))      => cmd_remove(&mut people, n),
+            Ok(Command::Show)           => cmd_show(&people),
+            Ok(Command::Help)           => cmd_help(),
+            Ok(Command::Quit)           => break,
+            Err(e)                      => Err(e),
         };
+
+        if let Err(e) = res {
+            println!("An error occurred: {:?}", e)
+        }
     }
 }
