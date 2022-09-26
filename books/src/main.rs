efb41d0ae1352e65607770cbbe79f9e5c876f50f
Nils Asmussen <nils@os.inf.tu-dresden.de>
2022-09-26 10:37:52 +0200

    Added solution to books exercise.

--- books/src/main.rs
+++ books/src/main.rs
@@ -1,9 +1,56 @@
 use std::io;
 use std::io::Write;
 
-struct Book {}
+struct Book {
+    title: String,
+    year: i32,
+}
 
-enum Command {}
+impl Book {
+    fn new(title: String) -> Book {
+        Book { title, year: 0 }
+    }
+}
+
+enum Command {
+    Add(String),
+    Year(String, String),
+    Remove(String),
+    Show,
+    Help,
+    Quit,
+}
+
+fn parse(cmd: String) -> Command {
+    let parts: Vec<&str> = cmd.split_whitespace().collect();
+
+    if parts.len() == 0 {
+        panic!("Unknown command");
+    }
+
+    match parts[0] {
+        "add" => {
+            assert!(parts.len() == 2);
+            Command::Add(parts[1].to_string())
+        }
+
+        "year" => {
+            assert!(parts.len() == 3);
+            Command::Year(parts[1].to_string(), parts[2].to_string())
+        }
+
+        "rem" => {
+            assert!(parts.len() == 2);
+            Command::Remove(parts[1].to_string())
+        }
+
+        "show" => Command::Show,
+        "help" => Command::Help,
+        "quit" => Command::Quit,
+
+        _ => panic!("Unknown command"),
+    }
+}
 
 fn get_cmd() -> Command {
     // print prompt (ignore errors)
@@ -12,8 +59,28 @@ fn get_cmd() -> Command {
 
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
-    // TODO parse line and create Command
-    unimplemented!();
+    parse(command)
+}
+
+fn cmd_add(books: &mut Vec<Book>, title: String) {
+    books.push(Book::new(title));
+}
+
+fn cmd_year(books: &mut Vec<Book>, title: String, year: String) {
+    let p = books.iter_mut().find(|p| p.title == title).unwrap();
+    p.year = year.parse::<i32>().unwrap();
+}
+
+fn cmd_remove(books: &mut Vec<Book>, title: String) {
+    books.retain(|p| p.title != title);
+}
+
+fn cmd_show(books: &Vec<Book>) {
+    println!("[");
+    for p in books {
+        println!("  {} (Year {})", p.title, p.year);
+    }
+    println!("]");
 }
 
 fn cmd_help() {
@@ -34,6 +101,13 @@ fn main() {
     loop {
         let command = get_cmd();
 
-        // TODO handle command
+        match command {
+            Command::Add(n) => cmd_add(&mut books, n),
+            Command::Year(n, y) => cmd_year(&mut books, n, y),
+            Command::Remove(n) => cmd_remove(&mut books, n),
+            Command::Show => cmd_show(&books),
+            Command::Help => cmd_help(),
+            Command::Quit => break,
+        };
     }
 }
