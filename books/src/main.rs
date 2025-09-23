fdbcbb80690bb1c726e3ced8a56924c11295fec4
Nils Asmussen <nils@os.inf.tu-dresden.de>
2025-09-23 16:06:47 +0200

    Added solution for books exercise.

--- books/src/main.rs
+++ books/src/main.rs
@@ -1,9 +1,10 @@
 use std::io;
 use std::io::Write;
+use std::num::ParseIntError;
 
 struct Book {
     title: String,
-    year: u32,
+    year: Option<u32>,
 }
 
 enum Command {
@@ -15,23 +16,42 @@ enum Command {
     Help,
 }
 
-fn get_cmd() -> Command {
+#[derive(Debug)]
+enum Error {
+    UnknownCommand,
+    ParseInt,
+    IOError,
+}
+
+impl From<std::io::Error> for Error {
+    fn from(_value: std::io::Error) -> Self {
+        Self::IOError
+    }
+}
+
+impl From<ParseIntError> for Error {
+    fn from(_value: ParseIntError) -> Self {
+        Self::ParseInt
+    }
+}
+
+fn get_cmd() -> Result<Command, Error> {
     // print prompt (ignore errors)
     print!("> ");
     io::stdout().flush().ok();
 
     let mut command = String::new();
-    io::stdin().read_line(&mut command).unwrap();
+    io::stdin().read_line(&mut command)?;
 
     let words: Vec<&str> = command.split_whitespace().collect();
     match words.as_slice() {
-        ["add", title] => Command::Add(title.to_string()),
-        ["year", title, year] => Command::Year(title.to_string(), year.parse().unwrap()),
-        ["rem", title] => Command::Rem(title.to_string()),
-        ["show"] => Command::Show,
-        ["help"] => Command::Help,
-        ["quit"] => Command::Quit,
-        _ => panic!("Unknown command"),
+        ["add", title] => Ok(Command::Add(title.to_string())),
+        ["year", title, year] => Ok(Command::Year(title.to_string(), year.parse()?)),
+        ["rem", title] => Ok(Command::Rem(title.to_string())),
+        ["show"] => Ok(Command::Show),
+        ["help"] => Ok(Command::Help),
+        ["quit"] => Ok(Command::Quit),
+        _ => Err(Error::UnknownCommand),
     }
 }
 
@@ -51,18 +71,34 @@ fn main() {
     println!("Welcome! Please enter a command.");
 
     loop {
-        let command = get_cmd();
+        let command = match get_cmd() {
+            Ok(cmd) => cmd,
+            Err(e) => {
+                println!("An error occurred: {:?}", e);
+                continue;
+            }
+        };
 
         match command {
-            Command::Add(title) => books.push(Book { title, year: 0 }),
+            Command::Add(title) => books.push(Book { title, year: None }),
             Command::Year(title, year) => {
-                let book = books.iter_mut().find(|b| b.title == title).unwrap();
-                book.year = year;
-            },
+                let book = match books.iter_mut().find(|b| b.title == title) {
+                    Some(book) => book,
+                    None => {
+                        println!("Book with title {} not found.", title);
+                        continue;
+                    }
+                };
+                book.year = Some(year);
+            }
             Command::Rem(title) => books.retain(|b| b.title != title),
             Command::Show => {
                 for b in &books {
-                    println!("{} ({})", b.title, b.year);
+                    if let Some(year) = b.year {
+                        println!("{} ({})", b.title, year);
+                    } else {
+                        println!("{}", b.title);
+                    }
                 }
             }
             Command::Help => cmd_help(),
