3c7665357232a21f0e699a82238715adffee7080
Nils Asmussen <nils@os.inf.tu-dresden.de>
2025-09-23 13:04:26 +0200

    Added solution for books exercise.

--- books/src/main.rs
+++ books/src/main.rs
@@ -1,9 +1,19 @@
 use std::io;
 use std::io::Write;
 
-struct Book {}
+struct Book {
+    title: String,
+    year: u32,
+}
 
-enum Command {}
+enum Command {
+    Add(String),
+    Year(String, u32),
+    Rem(String),
+    Show,
+    Quit,
+    Help,
+}
 
 fn get_cmd() -> Command {
     // print prompt (ignore errors)
@@ -12,8 +22,17 @@ fn get_cmd() -> Command {
 
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
-    // TODO parse line and create Command
-    unimplemented!();
+
+    let words: Vec<&str> = command.split_whitespace().collect();
+    match words.as_slice() {
+        ["add", title] => Command::Add(title.to_string()),
+        ["year", title, year] => Command::Year(title.to_string(), year.parse().unwrap()),
+        ["rem", title] => Command::Rem(title.to_string()),
+        ["show"] => Command::Show,
+        ["help"] => Command::Help,
+        ["quit"] => Command::Quit,
+        _ => panic!("Unknown command"),
+    }
 }
 
 fn cmd_help() {
@@ -34,6 +53,20 @@ fn main() {
     loop {
         let command = get_cmd();
 
-        // TODO handle command
+        match command {
+            Command::Add(title) => books.push(Book { title, year: 0 }),
+            Command::Year(title, year) => {
+                let book = books.iter_mut().find(|b| b.title == title).unwrap();
+                book.year = year;
+            },
+            Command::Rem(title) => books.retain(|b| b.title != title),
+            Command::Show => {
+                for b in &books {
+                    println!("{} ({})", b.title, b.year);
+                }
+            }
+            Command::Help => cmd_help(),
+            Command::Quit => break,
+        }
     }
 }
