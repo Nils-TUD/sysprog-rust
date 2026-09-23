d32cb1c760187b963988e6efbcff71754e82c837
Nils Asmussen <nils@os.inf.tu-dresden.de>
2026-09-23 10:37:21 +0200

    Added words solution.

--- words/src/lib.rs
+++ words/src/lib.rs
@@ -1,14 +1,27 @@
 /// Returns the number of upper case letters in `word`
 pub fn count_upper(word: &str) -> usize {
-    // TODO
-    0
+    let mut upper = 0;
+    for c in word.chars() {
+        if c.is_uppercase() {
+            upper += 1;
+        }
+    }
+    upper
 }
 
 /// Returns a tuple with the number of words in `text` that contain upper case letters and the total
 /// number of upper case letters
 pub fn count_words_upper(text: &str) -> (usize, usize) {
-    // TODO
-    (0, 0)
+    let mut total_upper = 0;
+    let mut upper_words = 0;
+    for w in text.split_whitespace() {
+        let upper = count_upper(w);
+        if upper > 0 {
+            total_upper += upper;
+            upper_words += 1;
+        }
+    }
+    (upper_words, total_upper)
 }
 
 #[test]
